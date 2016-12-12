4f1914ec11ddef860637e10957c30b143cc7a275
Shane Peckham <shanepeckham@live.com>
2016-12-12 17:34:18 +0000

    mysql handler

--- handlers/contacts.js
+++ handlers/contacts.js
@@ -2,8 +2,32 @@
 
  var repository = require('../lib/contactRepository');
 
+ var mysql = require('mysql');
+
+ var connection = mysql.createConnection(
+     {
+         host     : 'localhost',
+         user     : 'root',
+         password : 'CADSession2016',
+         database : 'mysql',
+     }
+ );
+
  module.exports = {
      get: function contacts_get(req, res) {
-         res.json(repository.all())
+
+         connection.connect();
+
+         var queryString = 'SELECT * FROM contacts';
+
+         connection.query(queryString, function(err, rows, fields) {
+             if (err) throw err;
+
+             for (var i in rows) {
+               //  console.log('Post Titles: ', rows[i].post_title);
+                 res(rows);
+             }
+         });
+     //    res.json(repository.all())
      }
  };
