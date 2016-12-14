43358a50dd3ed24308bb38d5091673247a603bff
Shane Peckham <shanepeckham@live.com>
2016-12-14 16:22:40 +0000

    Fixed SQL Typo2

--- handlers/contacts/{id}.js
+++ handlers/contacts/{id}.js
@@ -19,11 +19,12 @@
          connection.connect();
 
          var queryString = 'SELECT last_feedback FROM cases where contactId = " ' + req.params['id'] + '"';
-         //console.log(queryString);
+         console.log(queryString);
          connection.query(queryString, function(err, rows, fields) {
              if (err) throw err;
 
              for (var i in rows) {
+                 console.log(rows);
                  res.json(rows);
              }
          });
