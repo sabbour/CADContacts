289202017ea64916c61e90a8de6b9a9aea143006
Shane Peckham <shanepeckham@live.com>
2016-12-15 13:10:30 +0000

    remove whitespaces

--- handlers/contacts/{id}.js
+++ handlers/contacts/{id}.js
@@ -18,7 +18,7 @@
 
          connection.connect();
 
-         var queryString = 'SELECT last_feedback FROM cases where contactId = " ' + req.params['id'] + '"';
+         var queryString = 'SELECT last_feedback FROM cases where contactId = " ' + req.params['id'].replace(/\s+/, "")  + '"';
          console.log(queryString);
          connection.query(queryString, function(err, rows, fields) {
              if (err) throw err;
